e044a0280af266e2407f25489be96f2a28b6ab0a
saki <aoyama.s.730@gmail.com>
2023-12-01 22:34:29 +0900

    変更

---
+++ main.php
@@ -0,0 +1,22 @@
+<?php
+// javascriptから送られてきたクエリを変数に代入
+$latitude =  $_POST['latitude']; 
+$longitude = $_POST['longitude'];
+$range = $_POST['range'];
+$start = $_POST['start'];
+// クエリをまとめる
+$query = [
+    'key' => 'apikey', // <-- ここにあなたのAPIキーを設定してください。
+    'lat' => $latitude, // 緯度
+    'lng' => $longitude, // 経度
+    'range' => $range, // 検索範囲
+    'start' => $start, // 検索の開始位置
+    'format' => 'json', // レスポンス形式
+];
+// グルメサーチAPIからjsonを取得
+$url = 'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?';
+$url .= http_build_query($query);
+$response = file_get_contents($url);
+$json = json_encode($response);
+
+echo ($json);
